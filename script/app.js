63b2de1822826da97c300eb76a3b2fb8d8ab0770
crimme <kueppers.fabian@web.de>
2024-07-22 18:52:52 +0200

    update colors

--- script/app.js
+++ script/app.js
@@ -1,4 +1,4 @@
-async function getPokemonList(limit = 6, offset = 0) {
+async function getPokemonList(limit = 2, offset = 0) {
   let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
   let response = await fetch(url);
   let data = await response.json();
@@ -17,13 +17,15 @@ async function displayPokemonList(pokemonList) {
   const renderData = document.getElementById("gallery");
   renderData.innerHTML = "";
 
-  for (let pokemon of pokemonList) {
+  for (let j = 0; j < pokemonList.length; j++) {
+    let pokemon = pokemonList[j];
     let pokemonDetails = await getPokemonDetails(pokemon.url);
     renderData.innerHTML += `
     <div class="card">
-    <div class="cardHeader"><span>${pokemonDetails.name}</span> <span>#${pokemonDetails.id}</span></div>
-    <img src="${pokemonDetails.sprites.other.home.front_default}">
-    
+      <div class="cardHeader"><span>${pokemonDetails.name}</span> <span>#${pokemonDetails.id}</span></div>
+      <img src="${pokemonDetails.sprites.other.home.front_default}">
+      <span>${pokemonDetails.types[0].type.name}</span> <br>
+
     </div>`;
   }
 }
