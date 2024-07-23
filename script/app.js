3ed066fa7c4ee2fb3cd49f14a0335fd576943992
crimme <kueppers.fabian@web.de>
2024-07-23 14:40:59 +0200

    updates

--- script/app.js
+++ script/app.js
@@ -1,7 +1,7 @@
 let currentOffset = 0;
 const limit = 2;
 
-async function getPokemonList(limit = 20, offset = 0) {
+async function getPokemonList(limit = 2, offset = 0) {
   let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
   let response = await fetch(url);
   let data = await response.json();
@@ -25,10 +25,10 @@ async function displayPokemonList(pokemonList) {
     let pokemonDetails = await getPokemonDetails(pokemon.url);
     renderData.innerHTML += `
     <div class="card ${pokemonDetails.types[0].type.name}">
-      <div><span class="idNumber">#${pokemonDetails.id}</span></div>
+      <div class="idNumber"><span>#${pokemonDetails.id}</span></div>
       <img src="${pokemonDetails.sprites.other.home.front_default}">
       <div class="cardHeader"><span><b>${pokemonDetails.name}</b></span></div>
-      <div class="typeContainer"><p class="cardType ${pokemonDetails.types[0].type.name}">${pokemonDetails.types[0].type.name}</p></div> <br>
+      <div class="cardFooter"><div class="typeContainer"><p class="cardType ${pokemonDetails.types[0].type.name}">${pokemonDetails.types[0].type.name}</p></div></div> <br>
     </div>`;
   }
 }
